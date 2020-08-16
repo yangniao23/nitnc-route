da5231855bf5c49a71b9eb45b2552608c6df462f
yangniao23 <yangniao23@gmail.com>
2020-08-16 22:16:24 +0900

    Update: うごかない

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 // start clockbox
 function set2fig(num) {
     // 桁数が1桁だったら先頭に0を加えて2桁に調整する
-    var ret;
+    let ret;
     if( num < 10 ) { ret = "0" + num; }
     else { ret = num; }
     return ret;
@@ -15,8 +15,8 @@ function nowtime() {
     return [nowHour, nowMin, nowSec];
 }
  function showClock() {
-    time = nowtime();
-    var msg = "現在時刻: " + time[0] + "時" + time[1] + "分" + time[2] + "秒";
+    const time = nowtime();
+    const msg = "現在時刻: " + time[0] + "時" + time[1] + "分" + time[2] + "秒";
     document.getElementById("clockbox").innerHTML = msg;
  }
 
@@ -26,47 +26,38 @@ function nowtime() {
 
  // start transfer
 function getlatest() {
-    time = nowtime().join(',').replace(/,/g, '');
-    
-    document.getElementById("next_").innerHTML = next_;
+    const time = nowtime().join(',').replace(/,/g, '');
+
+    //document.getElementById("next_").innerHTML = next_;
+    document.getElementById("next_").innerHTML = time;
 }
 
 
  // end transfer
 
  // start setting
- /*
-function _create_dropdownlist(form_name, select_name, select_array) {
-	// length
-	document[form_name][select_name].length = select_array.length;
-	
-	// text, value
-	var i;
-	for ( i=0; i<select_array.length; i++){
-		document[form_name][select_name].options[i].text = select_array[i].text;
-		document[form_name][select_name].options[i].value = select_array[i].value;
-	}
+function gettimetablefilename(data, startpoint, endpoint) {
+    for(i in data) {
+        if(data[i].stop.indexOf(startpoint) < data[i].stop.indexOf(endpoint)) {
+                console.log(data[i].stop);
+        }
+        
+    }
 }
-
-args = [
-    {text:"長岡", value:0},
-    {text:"北長岡", value:1}
-]
-_create_dropdownlist(form_ ,start_place, args)
-*/
  // end setting
 
  // start modules
 function readjson(path) {
-    let req = new XMLHttpRequest();						// XMLHttpRequest オブジェクトを生成する
-	req.onreadystatechange = function() {				// XMLHttpRequest オブジェクトの状態が変化した際に呼び出されるイベントハンドラ
-		if(req.readyState == 4 && req.status == 200){	// サーバーからのレスポンスが完了し、かつ、通信が正常に終了した場合
-
-			return JSON.parse(req.responseText);
-		}
-	};
-	req.open("GET", path, false);				// HTTPメソッドとアクセスするサーバーのURLを指定
-	req.send(null);										// 実際にサーバーへリクエストを送信
+    const req = new XMLHttpRequest();
+    req.onreadystatechange = function() {
+        if(req.readyState == 4 && req.status == 200){
+            console.log(req.responseText);
+        }
+    };
+    req.open("GET", path, false);
+    req.send(null);
 }
+
+
 
  // end modules
